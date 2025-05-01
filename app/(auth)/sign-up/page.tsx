d3d257fc0e41b351e650761e8f2f58be08b96bc1
Ritwik Pai <ritwikpai777@gmail.com>
2025-05-01 16:09:46 +0530

    setup app, file and folder structure, routing and auth pages

---
+++ app/(auth)/sign-up/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import AuthForm from "@/app/components/AuthForm";
+
+const Page = () => {
+  return (
+    <>
+      <AuthForm type="sign-up" />
+    </>
+  );
+};
+
+export default Page;
