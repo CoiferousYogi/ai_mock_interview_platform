211d7b335dfea268c90413dd93d103e9fcc3ddf8
Ritwik Pai <ritwikpai777@gmail.com>
2025-05-06 00:44:15 +0530

    implemented interview-ai

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -2,10 +2,23 @@ import { Button } from "@/components/ui/button";
 import Link from "next/link";
 import React from "react";
 import Image from "next/image";
-import { dummyInterviews } from "@/constants";
 import InterviewCard from "@/components/ui/InterviewCard";
+import { getCurrentUser } from "@/lib/actions/auth.action";
+import {
+  getInterviewsByUserId,
+  getLatestInterviews,
+} from "@/lib/actions/general.action";
 
-const Page = () => {
+const Page = async () => {
+  const user = await getCurrentUser();
+
+  const [userInterviews, allInterviews] = await Promise.all([
+    await getInterviewsByUserId(user?.id!),
+    await getLatestInterviews({ userId: user?.id! }),
+  ]);
+
+  const hasPastInterviews = userInterviews?.length > 0;
+  const hasUpcomingInterviews = allInterviews?.length > 0;
   return (
     <>
       <section className="card-cta">
@@ -34,9 +47,13 @@ const Page = () => {
         <h2>Your Interviews</h2>
 
         <div className="interviews-section">
-          {dummyInterviews?.map((interview) => (
-            <InterviewCard {...interview} key={interview.id} />
-          ))}
+          {hasPastInterviews ? (
+            userInterviews?.map((interview) => (
+              <InterviewCard {...interview} key={interview.id} />
+            ))
+          ) : (
+            <p>You have&apos;t taken any interviews yet</p>
+          )}
         </div>
       </section>
 
@@ -44,10 +61,13 @@ const Page = () => {
         <h2>Take an Interview</h2>
 
         <div className="interviews-section">
-          {dummyInterviews?.map((interview) => (
-            <InterviewCard {...interview} key={interview.id} />
-          ))}
-          <p>There are no interviews available</p>
+          {hasUpcomingInterviews ? (
+            allInterviews?.map((interview) => (
+              <InterviewCard {...interview} key={interview.id} />
+            ))
+          ) : (
+            <p>There are no new interviews available</p>
+          )}
         </div>
       </section>
     </>
